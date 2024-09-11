56dcce87552891e2f86ee268cb9cc87d5ccf1e80
LesterFlores <flesterj@miumg.edu.gt>
2024-09-10 23:42:22 -0600

    alerta de contraseña incorrecta

--- Login/index.php
+++ Login/index.php
@@ -8,18 +8,37 @@
 </head>
 <body>
   <div class="container">
-    <form action="../acciones/usuarios.php">
+    <form action="../acciones/usuarios.php" method="GET">
       <div class="form-group">
-        <label for="email">Correo electronico</label>
-        <input type="email" id="email" name="email">
+        <label for="email">Correo electrónico</label>
+        <input type="email" id="email" name="email" required>
       </div>
       <div class="form-group">
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" name="password">
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" name="password" required>
       </div>
       <button type="submit">Iniciar sesión</button>
     </form>
-    <p><a href="#">Olvidé mi contraseña?</a></p>
+    <p><a href="#">¿Olvidé mi contraseña?</a></p>
   </div>
+
+  <!-- Aquí se inyecta la alerta emergente si hay error -->
+  <?php
+  if (isset($_GET['alerta'])) {
+    if ($_GET['alerta'] == 'error') {
+      echo "
+        <script>
+          alert('Credenciales incorrectas. Por favor, intente nuevamente.');
+        </script>
+      ";
+    } elseif ($_GET['alerta'] == 'desactivado') {
+      echo "
+        <script>
+          alert('Su cuenta está desactivada. Por favor, contacte al administrador.');
+        </script>
+      ";
+    }
+  }
+  ?>
 </body>
 </html>
